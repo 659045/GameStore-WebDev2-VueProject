a270ccef8142e83aef6a69d1ebebd518eb0f389b
Jason Xie <659045@student.inholland.nl>
2025-06-19 22:13:36 +0200

    update date

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="d flex align-items-center justify-content-center text-center py-3 border-top bg-dark ">
-        <p class="col-md-4 mb-0">©2023 Jason's Game Store</p>
+        <p class="col-md-4 mb-0">©2025 Jason's Game Store</p>
     </footer>
 </template>
 
